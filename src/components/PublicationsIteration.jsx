d38abc8fa373df30e3dbda16e610bf27ce156448
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-04-28 21:16:42 +0200

    removed repeated code by condional  rendering

--- src/components/PublicationsIteration.jsx
+++ src/components/PublicationsIteration.jsx
@@ -87,211 +87,108 @@ export default function PublicationsIteration({ project, all }) {
         publications: groupPublicationsByYearAndType(filteredPublications)[year][type],
       })),
     }));
-  return (
-    <div className="mx-auto mt-10 max-w-2xl border-t border-gray-200 pt-4 sm:mt-16  lg:mx-0 lg:max-w-none flex-col justfify-center items-center ">
-      {all == false
-        ? groupedPublicationsOfProject
-        .map((publication, index) => {
-          return (
-          <>
-            <div key={index}>
-              <div className="flex justify-center w-full shadow-2xl mb-3">
-                <span className="text-3xl font-bold tracking-tight text-gray-900 gradient mb-5">
-                  {publication.year}
-                </span>
-              </div>
-              {publication.types.map((typeItem, subIndex) => (
-                <div key={subIndex}>
-                  <h2 className="text-xl font-bold text-gray-900 mb-3 ml-3">
-                    {typeItem.type}:
-                  </h2>
-                  {typeItem.publications.map((pub, innerIndex) => (
-                    <article
-                      key={innerIndex}
-                      className="flex max-w-4xl flex-col items-start justify-between mx-auto mb-10"
-                    >
-                      <div className="flex items-center gap-x-4 text-xs">
-                        <time
-                          dateTime={pub.datetime}
-                          className="text-gray-500"
-                        >
-                          {pub.dateTime}
-                        </time>
-                      </div>
-                      <div className="group relative w-full">
-                        <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                          <div className="text-2xl">
-                            <span className="absolute inset-0" />
-                            {pub.title}
-                          </div>
-                        </h3>
-                        <h5 style={{ display: "inline" }}>Authors:</h5>
-                        <p style={{ display: "inline" }}> {pub.authors}</p>
-                        <br></br>
-                        <h5 style={{ display: "inline" }}>Venue:</h5>
-                        <p style={{ display: "inline" }}> {pub.venue}</p>
-                      </div>
-                      <div className="flex justify-end items-center gap-3 w-full mb-3">
-                        {pub.pdfPathHref ? (
-                          <a
-                            href={pub.pdfPathHref}
-                            className="flex justify-center items-center hover:scale-105 duration-300"
-                          >
-                            <FaRegFilePdf size={20} /> <span>PDF</span>
-                          </a>
-                        ) : null}
-                        {pub.datasetLink ? (
-                          <a
-                            className="flex justify-center items-center hover:scale-105 duration-300"
-                            href={pub.datasetLink}
-                          >
-                            <GoDatabase size={20} /> <span>Database</span>
-                          </a>
-                        ) : null}
-                        {pub.githubLink ? (
-                          <a
-                            className="flex justify-center items-center hover:scale-105 duration-300"
-                            href={pub.githubLink}
-                          >
-                            <FaGithub size={20} /> <span>Github</span>
-                          </a>
-                        ) : null}
-                        {pub.presentationLink ? (
-                          <a
-                            className="flex justify-center items-center hover:scale-105 duration-300"
-                            href={pub.presentationLink}
-                          >
-                            <LuPresentation size={20} />{" "}
-                            <span>Presentation </span>
-                          </a>
-                        ) : null}
 
-                        {/* Toggle Text Area button */}
-                        <div
-                          className="flex justify-center items-center hover:scale-105 duration-300 text-[#868fa9] cursor-pointer"
-                          onClick={() => toggleTextArea(innerIndex)}
-                        >
-                          <FaQuoteLeft />
-                          <span>BibTex</span>
-                        </div>
+  const ChosenPublication = all == false? groupedPublicationsOfProject : groupedPublications
+  return (
+    <div className="mx-auto mt-10 max-w-2xl border-t border-gray-200 pt-4 sm:mt-16  lg:mx-0 lg:max-w-none flex-col justfify-center items-center">
+      {ChosenPublication.map((publication, index) => (
+        <div key={index}>
+          <div className="flex justify-center w-full shadow-2xl mb-3">
+            <span className="text-3xl font-bold tracking-tight text-gray-900 gradient mb-5">
+              {publication.year}
+            </span>
+          </div>
+          {publication.types.map((typeItem, subIndex) => (
+            <div key={subIndex}>
+              <h2 className="text-xl font-bold text-gray-900 mb-3 ml-3">
+                {typeItem.type}:
+              </h2>
+              {typeItem.publications.map((pub, innerIndex) => (
+                <article
+                  key={innerIndex}
+                  className="flex max-w-4xl flex-col items-start justify-between mx-auto mb-10"
+                >
+                  <div className="flex items-center gap-x-4 text-xs">
+                    <time
+                      dateTime={pub.datetime}
+                      className="text-gray-500"
+                    >
+                      {pub.dateTime}
+                    </time>
+                  </div>
+                  <div className="group relative w-full">
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                      <div className="text-2xl">
+                        <span className="absolute inset-0" />
+                        {pub.title}
                       </div>
-                      {/* Pre-defined text area */}
-                      {showTextAreas[innerIndex] && (
-                        <textarea
-                          className="rounded-[7px] border border-gray-400 flex justify-center w-full"
-                          rows={4}
-                          cols={50}
-                          value={convertToBibTeX(pub)}
-                          readOnly
-                        />
-                      )}
-                    </article>
-                  ))}
-                </div>
+                    </h3>
+                    <h5 style={{ display: "inline" }}>Authors:</h5>
+                    <p style={{ display: "inline" }}> {pub.authors}</p>
+                    <br />
+                    <h5 style={{ display: "inline" }}>Venue:</h5>
+                    <p style={{ display: "inline" }}> {pub.venue}</p>
+                  </div>
+                  <div className="flex justify-end items-center gap-3 w-full mb-3">
+                    {pub.pdfPathHref && (
+                      <a
+                        href={pub.pdfPathHref}
+                        className="flex justify-center items-center hover:scale-105 duration-300"
+                      >
+                        <FaRegFilePdf size={20} /> <span>PDF</span>
+                      </a>
+                    )}
+                    {pub.datasetLink && (
+                      <a
+                        className="flex justify-center items-center hover:scale-105 duration-300"
+                        href={pub.datasetLink}
+                      >
+                        <GoDatabase size={20} /> <span>Database</span>
+                      </a>
+                    )}
+                    {pub.githubLink && (
+                      <a
+                        className="flex justify-center items-center hover:scale-105 duration-300"
+                        href={pub.githubLink}
+                      >
+                        <FaGithub size={20} /> <span>Github</span>
+                      </a>
+                    )}
+                    {pub.presentationLink && (
+                      <a
+                        className="flex justify-center items-center hover:scale-105 duration-300"
+                        href={pub.presentationLink}
+                      >
+                        <LuPresentation size={20} />{" "}
+                        <span>Presentation </span>
+                      </a>
+                    )}
+  
+                    {/* Toggle Text Area button */}
+                    <div
+                      className="flex justify-center items-center hover:scale-105 duration-300 text-[#868fa9] cursor-pointer"
+                      onClick={() => toggleTextArea(innerIndex)}
+                    >
+                      <FaQuoteLeft />
+                      <span>BibTex</span>
+                    </div>
+                  </div>
+                  {/* Pre-defined text area */}
+                  {showTextAreas[innerIndex] && (
+                    <textarea
+                      className="rounded-[7px] border border-gray-400 flex justify-center w-full"
+                      rows={4}
+                      cols={50}
+                      value={convertToBibTeX(pub)}
+                      readOnly
+                    />
+                  )}
+                </article>
               ))}
             </div>
-          </>
-        )})
-        : groupedPublications.map((publication, index) => (
-            <>
-              <div key={index}>
-                <div className="flex justify-center w-full shadow-2xl mb-3">
-                  <span className="text-3xl font-bold tracking-tight text-gray-900 gradient mb-5">
-                    {publication.year}
-                  </span>
-                </div>
-                {publication.types.map((typeItem, subIndex) => (
-                  <div key={subIndex}>
-                    <h2 className="text-xl font-bold text-gray-900 mb-3 ml-3">
-                      {typeItem.type}:
-                    </h2>
-                    {typeItem.publications.map((pub, innerIndex) => (
-                      <article
-                        key={innerIndex}
-                        className="flex max-w-4xl flex-col items-start justify-between mx-auto mb-10"
-                      >
-                        <div className="flex items-center gap-x-4 text-xs">
-                          <time
-                            dateTime={pub.datetime}
-                            className="text-gray-500"
-                          >
-                            {pub.dateTime}
-                          </time>
-                        </div>
-                        <div className="group relative w-full">
-                          <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                            <div className="text-2xl">
-                              <span className="absolute inset-0" />
-                              {pub.title}
-                            </div>
-                          </h3>
-                          <h5 style={{ display: "inline" }}>Authors:</h5>
-                          <p style={{ display: "inline" }}> {pub.authors}</p>
-                          <br></br>
-                          <h5 style={{ display: "inline" }}>Venue:</h5>
-                          <p style={{ display: "inline" }}> {pub.venue}</p>
-                        </div>
-                        <div className="flex justify-end items-center gap-3 w-full mb-3">
-                          {pub.pdfPathHref ? (
-                            <a
-                              href={pub.pdfPathHref}
-                              className="flex justify-center items-center hover:scale-105 duration-300"
-                            >
-                              <FaRegFilePdf size={20} /> <span>PDF</span>
-                            </a>
-                          ) : null}
-                          {pub.datasetLink ? (
-                            <a
-                              className="flex justify-center items-center hover:scale-105 duration-300"
-                              href={pub.datasetLink}
-                            >
-                              <GoDatabase size={20} /> <span>Database</span>
-                            </a>
-                          ) : null}
-                          {pub.githubLink ? (
-                            <a
-                              className="flex justify-center items-center hover:scale-105 duration-300"
-                              href={pub.githubLink}
-                            >
-                              <FaGithub size={20} /> <span>Github</span>
-                            </a>
-                          ) : null}
-                          {pub.presentationLink ? (
-                            <a
-                              className="flex justify-center items-center hover:scale-105 duration-300"
-                              href={pub.presentationLink}
-                            >
-                              <LuPresentation size={20} />{" "}
-                              <span>Presentation </span>
-                            </a>
-                          ) : null}
-
-                          {/* Toggle Text Area button */}
-                          <div
-                            className="flex justify-center items-center hover:scale-105 duration-300 text-[#868fa9] cursor-pointer"
-                            onClick={() => toggleTextArea(innerIndex)}
-                          >
-                            <FaQuoteLeft />
-                            <span>BibTex</span>
-                          </div>
-                        </div>
-                        {/* Pre-defined text area */}
-                        {showTextAreas[innerIndex] && (
-                          <textarea
-                            className="rounded-[7px] border border-gray-400 flex justify-center w-full"
-                            rows={4}
-                            cols={50}
-                            value={convertToBibTeX(pub)}
-                            readOnly
-                          />
-                        )}
-                      </article>
-                    ))}
-                  </div>
-                ))}
-              </div>
-            </>
           ))}
+        </div>
+      ))}
     </div>
   );
 }
