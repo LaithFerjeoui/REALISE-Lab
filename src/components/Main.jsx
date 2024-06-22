4e335aff2d4d79414d960a7f54035ae597592dfe
Meryam Rakkez <rakkezmeryam@gmail.com>
2024-06-22 19:07:41 +0100

    header bg update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,7 +9,7 @@ export default function Example() {
   return (
     <Layout>
       {/* FIRST CONTAINER HERE */}
-      <div className="  h-[420px]  bg-gradient-to-b from-gray-400 to-white  ">
+      <div className="h-[420px] bg-gradient-to-b from-gray-400 to-white/5">
         <div className="flex flex-col justify-center items-center pt-12">
           <img className="w-[150px]" src="img/logo.png" alt="REALISE" />
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl gradient  text-center">
