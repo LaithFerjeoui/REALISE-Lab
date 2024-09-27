849e64b663fb161eef576be0bc7d4f199244efb0
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-09-27 21:07:21 +0200

    fixed

--- src/components/PartnersOnHomePage.jsx
+++ src/components/PartnersOnHomePage.jsx
@@ -8,15 +8,15 @@ const PartnersMainPage = () => {
 
 
   return (
-        <div className="mx-auto flex justify-center w-full">
-          <div className="mx-auto mt-10 flex justify-center items-center gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:pt-16 w-full ">
+        <div className="mx-auto flex justify-center  w-full">
+          <div className="mx-auto mt-10 flex justify-center flex-wrap items-center gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:pt-16 w-full ">
             {partners.map((partner) => (
-              <article key={partner.name} className="flex max-w-xl flex-col items-start justify-between">
+              <article key={partner.name} className="flex max-w-lg flex-col items-start justify-between">
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                     <a href={partner.link} target="_blank" rel="noopener noreferrer">
                       <span className="absolute inset-0 border" />
-                      <img className="mx-auto " src={partner.imagePath} alt=""/>
+                      <img className="mx-auto  " src={partner.imagePath} alt=""/>
                     </a>
                   </h3>
                 </div>
